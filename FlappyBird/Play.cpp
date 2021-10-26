8054b933b7339725c79c2a3d6ad9f9aac53b9fad
martin <martinconcetti97@gmail.com>
2021-10-25 22:34:51 -0300

    Plat screen implemented

--- FlappyBird/Play.cpp
+++ FlappyBird/Play.cpp
@@ -1,1 +1,30 @@
 #include "Play.h"
+
+Play::Play() 
+{
+
+}
+
+void Play::setSceneManager(sceneManager* manager)
+{
+	this->manager = manager;
+}
+
+Play::~Play()
+{
+
+}
+
+void Play::playUpdate()
+{
+	
+}
+
+void Play::playDraw()
+{
+	BeginDrawing();
+	ClearBackground(RED);
+	DrawTextEx(GetFontDefault(), "Funco", { (float)GetScreenWidth() / 4, (float)GetScreenHeight() / 6 }, 30.0f, 5.0f, YELLOW);
+	
+	EndDrawing();
+}
